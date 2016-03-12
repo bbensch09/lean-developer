cf658d46c350f3ab220e6b36b03d42453f4ea372
Brian <bbensch@gmail.com>
2016-03-12 12:54:57 -0800

    ensure env variables are appropriately loading, confirmed snippets mailer working as intended

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def thank_you_for_snippet(snippet)
     @snippet = snippet
-    mail(to: current_user.email, subject: "We appreciate your interest!")
+    mail(to: @snippet.email, subject: "We appreciate your interest!")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  #GMAIL CONFIG (per http://usingname.space/2015/07/25/gmail-smtp-ruby-on-rails-actionmailer-and-you/)
+  #GMAIL CONFIG
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
