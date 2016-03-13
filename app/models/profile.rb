ed31172ba1d7237bbb0a2cfa5209d53f178cc62c
Brian <bbensch@gmail.com>
2016-03-12 21:27:18 -0800

    refactor / cleanup extra AWS code

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,12 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, styles: { large: "400x400>", thumb: "80x80>" },  default_url: "/images/:style/missing.png",
           :storage => :s3,
-          :s3_credentials => {
-              :bucket => ENV['S3_BUCKET_NAME'],
-              :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-              :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-              },
-           :bucket => 'citizen-debate',
            :url => ':s3_domain_url',
            :path => '/:class/:attachment/:id_partition/:style/:filename',
            :s3_host_name => 's3-us-west-2.amazonaws.com'
