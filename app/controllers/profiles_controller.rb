d90fb69eb074bb06f4660bf98bac69e6bee14ff6
Brian <bbensch@gmail.com>
2016-03-13 14:13:51 -0700

    ranking of profile pages working as intended

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
   # GET /profiles
   # GET /profiles.json
   def index
-    @profiles = Profile.all
+    @profiles = Profile.all.sort { |a,b| b.user.points <=> a.user.points}
   end
 
   # GET /profiles/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :snippets
   has_one :profile
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Need to activate Omniauthabl to use FB still :omniauthable
+  # Also need to reactivate :confirmable to resume email confirmations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable, :timeoutable#, :confirmable
 
@@ -24,13 +25,14 @@ class User < ActiveRecord::Base
 
   def points
     #free points
-    #all users get a point for every user that signs up after them, so with 10 users, the 1st user gets 10 points, and the last user gets zero
+    #users get more points for being earlier to sign up; how it works: all users get a point for every user that signs up after them, so with 10 users, the 1st user gets 10 points, and the last user gets zero. when 11th user signs up, each of those users gets one more point.
     free_points = User.count - self.id
     total_points = free_points + self.profile_bonus + self.snippet_bonus
   end
 
   def rank
     all_users = User.all
+    #sort users by most points to least points
     all_users_sorted = all_users.sort { |a,b| b.points <=> a.points}
     all_users_sorted.index(self) + 1
   end
